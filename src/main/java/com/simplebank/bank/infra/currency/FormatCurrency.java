86dd0bb0943e46d656a605692417eb848c0d115d
Pedro <pedromf34@outlook.com>
2025-03-17 22:45:10 -0300

    fix: code style

--- src/main/java/com/simplebank/bank/infra/currency/FormatCurrency.java
+++ src/main/java/com/simplebank/bank/infra/currency/FormatCurrency.java
@@ -8,7 +8,7 @@ public class FormatCurrency
   public static String format(double value)
   {
     NumberFormat formatter = NumberFormat.getCurrencyInstance();
-    
+
     return formatter.format(value);
   }
 }
