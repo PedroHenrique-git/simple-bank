3d2814ad78683374cceb4bcf5e7e3d6459a91757
Pedro <pedromf34@outlook.com>
2025-03-25 23:03:28 -0300

    feat: authenticated user usecase test

--- src/test/java/com/simplebank/bank/usecases/AuthenticatedUserUseCaseTest.java
+++ src/test/java/com/simplebank/bank/usecases/AuthenticatedUserUseCaseTest.java
@@ -4,10 +4,21 @@ import com.simplebank.bank.config.AccountConfig;
 import com.simplebank.bank.config.CommonConfig;
 import com.simplebank.bank.config.TransactionConfig;
 import com.simplebank.bank.config.UserConfig;
+import com.simplebank.bank.domain.exceptions.ForbiddenException;
+import com.simplebank.bank.domain.exceptions.UseCaseException;
+import com.simplebank.bank.mocks.UserMock;
+import com.simplebank.bank.usecases.ports.AuthAuthenticatedUserDTORequest;
+import com.simplebank.bank.usecases.ports.AuthAuthenticatedUserDTOResponse;
+import com.simplebank.bank.usecases.ports.AuthManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,4 +27,20 @@ import org.springframework.test.context.ActiveProfiles;
     CommonConfig.class})
 public class AuthenticatedUserUseCaseTest
 {
+  @Autowired
+  public UseCase<AuthAuthenticatedUserDTORequest, AuthAuthenticatedUserDTOResponse> usecase;
+
+  @MockitoBean
+  AuthManager authManager;
+
+  @Test
+  void testAuthenticatedUser() throws ForbiddenException, UseCaseException
+  {
+    when(authManager.getAuthenticatedUser()).thenReturn(UserMock.createClientUser());
+
+    var authenticatedUser = usecase.execute(new AuthAuthenticatedUserDTORequest());
+
+    assertEquals("test-user@email.com", authenticatedUser.email());
+    assertEquals("test", authenticatedUser.name());
+  }
 }
