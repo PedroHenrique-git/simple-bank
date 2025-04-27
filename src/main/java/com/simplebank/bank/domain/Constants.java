3cf68d8784d9c8d16a03ab08e7f0cbaae398921a
Pedro <pedromf34@outlook.com>
2025-04-26 23:54:36 -0300

    feat: update auth token expiration

--- src/main/java/com/simplebank/bank/domain/Constants.java
+++ src/main/java/com/simplebank/bank/domain/Constants.java
@@ -2,7 +2,7 @@ package com.simplebank.bank.domain;
 
 public class Constants
 {
-  public static final long AUTH_TOKEN_EXPIRATION = 60; // minutes;
+  public static final long AUTH_TOKEN_EXPIRATION = 5; // minutes;
   public static final long REFRESH_TOKEN_EXPIRATION = 24 * 60 * 7; // Days
   public static final long REFRESH_TOKEN_COOKIE_EXPIRATION = 7; // Days
   public static final String REFRESH_TOKEN_COOKIE_NAME = "session";
